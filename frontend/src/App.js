3349734f7833157e6360ff0986335a5a08726b8a
PranavMunot <pranavmunot09@gmail.com>
2021-11-19 16:49:30 +0530

    All Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Footer from "./SharedComponent/Footer/Footer";
 import Header from "./SharedComponent/Header/Header";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="app">
-        
+        <Typography variant="h4">Application Page</Typography>
       </div>
       <Footer />
     </div>
