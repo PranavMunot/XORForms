e39349be00b2eb2e6b3d6af77d86d85806251d9a
Sk Moheuddin <moheuddin_s@India.XoriantCorp.com>
2021-11-24 09:59:13 +0530

    taking new code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,38 +2,25 @@ import { createTheme, ThemeProvider } from "@mui/material";
 import "./App.css";
 import Footer from "./SharedComponent/Footer/Footer";
 import Header from "./SharedComponent/Header/Header";
-<<<<<<< HEAD
 import Sidenav from "./SharedComponent/SideNav/SideNav";
 
 const ThemeColor = createTheme({
-  palette:{
-    primary:{
+  palette: {
+    primary: {
       main: '#12916f'
     }
   }
 })
-=======
-import { Typography } from "@mui/material";
->>>>>>> 3349734f7833157e6360ff0986335a5a08726b8a
+
 
 function App() {
   return (
-    
     <div className="App">
-<<<<<<< HEAD
-     <ThemeProvider theme={ThemeColor}>
-     <Header />
-      <div className="app"></div>
-=======
-      <Header />
-      <div className="app">
-        <Typography variant="h4">Application Page</Typography>
-      </div>
->>>>>>> 3349734f7833157e6360ff0986335a5a08726b8a
-      <Footer />
-</ThemeProvider>
-      
-
+      <ThemeProvider theme={ThemeColor}>
+        <Header />
+        <div className="app"></div>
+        <Footer />
+      </ThemeProvider>
     </div>
   );
 }
